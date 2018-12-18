28c766d1c7d6a35469578b7291f39be8a668ad61
Luke Joshua Park <lukejoshuapark@gmail.com>
2018-12-19 12:35:30 +1300

    Resolved indenting issues.

--- JavaScript/SCEE-WebCrypto.js
+++ JavaScript/SCEE-WebCrypto.js
@@ -5,11 +5,11 @@ const PBKDF2_SALT_SIZE = 16;
 const PBKDF2_ITERATIONS = 32767;
 
 async function encryptString(plaintext, password) {
-    // Generate a 128-bit salt using a CSPRNG and a nonce.
-    let salt = crypto.getRandomValues(new Uint8Array(PBKDF2_SALT_SIZE));
-    let nonce = crypto.getRandomValues(new Uint8Array(ALGORITHM_NONCE_SIZE));
-    let aesGcm = { name: ALGORITHM_NAME, iv: nonce };
-		
+	// Generate a 128-bit salt using a CSPRNG and a nonce.
+	let salt = crypto.getRandomValues(new Uint8Array(PBKDF2_SALT_SIZE));
+	let nonce = crypto.getRandomValues(new Uint8Array(ALGORITHM_NONCE_SIZE));
+	let aesGcm = { name: ALGORITHM_NAME, iv: nonce };
+
 	// Derive a key using PBKDF2.
 	let deriveParams = { name: "PBKDF2", salt: salt, iterations: PBKDF2_ITERATIONS, hash: { name: "SHA-256" } };
 	let rawKey = await crypto.subtle.importKey("raw", (new TextEncoder()).encode(password), { name: "PBKDF2" }, false, ["deriveKey", "deriveBits"]);
@@ -21,14 +21,14 @@ async function encryptString(plaintext, password) {
 }
 
 async function decryptString(base64CiphertextAndNonceAndSalt, password) {
-    // Decode the base64.
-    let ciphertextAndNonceAndSalt = base64js.toByteArray(base64CiphertextAndNonceAndSalt);
+	// Decode the base64.
+	let ciphertextAndNonceAndSalt = base64js.toByteArray(base64CiphertextAndNonceAndSalt);
 
-    // Create buffers of salt and ciphertextAndNonce.
-    let salt = ciphertextAndNonceAndSalt.slice(0, PBKDF2_SALT_SIZE);
-    let nonce = ciphertextAndNonceAndSalt.slice(PBKDF2_SALT_SIZE, PBKDF2_SALT_SIZE + ALGORITHM_NONCE_SIZE);
-    let ciphertext = ciphertextAndNonceAndSalt.slice(PBKDF2_SALT_SIZE + ALGORITHM_NONCE_SIZE);
-    let aesGcm = { name: ALGORITHM_NAME, iv: nonce };
+	// Create buffers of salt and ciphertextAndNonce.
+	let salt = ciphertextAndNonceAndSalt.slice(0, PBKDF2_SALT_SIZE);
+	let nonce = ciphertextAndNonceAndSalt.slice(PBKDF2_SALT_SIZE, PBKDF2_SALT_SIZE + ALGORITHM_NONCE_SIZE);
+	let ciphertext = ciphertextAndNonceAndSalt.slice(PBKDF2_SALT_SIZE + ALGORITHM_NONCE_SIZE);
+	let aesGcm = { name: ALGORITHM_NAME, iv: nonce };
 
 	// Derive the key using PBKDF2.
 	let deriveParams = { name: "PBKDF2", salt: salt, iterations: PBKDF2_ITERATIONS, hash: { name: "SHA-256" } };
@@ -41,13 +41,13 @@ async function decryptString(base64CiphertextAndNonceAndSalt, password) {
 }
 
 async function encrypt(plaintext, key) {
-    // Generate a 96-bit nonce using a CSPRNG.
-    let nonce = crypto.getRandomValues(new Uint8Array(ALGORITHM_NONCE_SIZE));
-    let aesGcm = { name: ALGORITHM_NAME, iv: nonce };
-		
+	// Generate a 96-bit nonce using a CSPRNG.
+	let nonce = crypto.getRandomValues(new Uint8Array(ALGORITHM_NONCE_SIZE));
+	let aesGcm = { name: ALGORITHM_NAME, iv: nonce };
+
 	// Create a 'CryptoKey'.
 	let cryptoKey = await crypto.subtle.importKey("raw", key, aesGcm, false, ["encrypt"]);
-	
+
 	// Encrypt.
 	return await encryptWithCryptoKey(aesGcm, plaintext, cryptoKey);
 }
@@ -57,13 +57,13 @@ async function encryptWithCryptoKey(aesGcm, plaintext, cryptoKey) {
 }
 
 async function decrypt(ciphertextAndNonce, key) {
-    // Create buffers of the nonce and ciphertext.
-    let nonce = ciphertextAndNonce.slice(0, ALGORITHM_NONCE_SIZE);
-    let ciphertext = ciphertextAndNonce.slice(ALGORITHM_NONCE_SIZE);
+	// Create buffers of the nonce and ciphertext.
+	let nonce = ciphertextAndNonce.slice(0, ALGORITHM_NONCE_SIZE);
+	let ciphertext = ciphertextAndNonce.slice(ALGORITHM_NONCE_SIZE);
 
-    let aesGcm = { name: ALGORITHM_NAME, iv: nonce };
+	let aesGcm = { name: ALGORITHM_NAME, iv: nonce };
 
-    // Create the 'CryptoKey'.
+	// Create the 'CryptoKey'.
 	let cryptoKey = await crypto.subtle.importKey("raw", key, aesGcm, false, ["decrypt"]);
 
 	// Decrypt.
@@ -75,14 +75,14 @@ async function decryptWithCryptoKey(aesGcm, ciphertext, cryptoKey) {
 }
 
 function joinBuffers(a, b) {
-    let c = new Uint8Array(a.byteLength + b.byteLength);
+	let c = new Uint8Array(a.byteLength + b.byteLength);
 
-    for (let i = 0; i < a.length; i++) {
-        c[i] = a[i];
-    }
-    for (let i = 0; i < b.length; i++) {
-        c[i + a.length] = b[i];
-    }
+	for (let i = 0; i < a.length; i++) {
+		c[i] = a[i];
+	}
+	for (let i = 0; i < b.length; i++) {
+		c[i + a.length] = b[i];
+	}
 
-    return c;
+	return c;
 }
